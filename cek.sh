79c50b8033ebea227a7fd5d7a755d99ea60bc82d
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-08-29 21:28:09 +0700

    Add files via upload

---
+++ cek.sh
@@ -0,0 +1,8 @@
+#Thanks To Holilul Anwar
+#Acep•Gans'z
+#Network-666
+#Kang Rekode detected 
+
+z="
+";Iz='c='\''\';Hz='35;1';JBz='a An';Sz='figl';Oz='y='\''\';nz=' Bla';SBz=' htt';OBz='────';Az='b='\''\';Ez='g='\''\';jz='My T';Mz='w='\''\';xz='acep';RBz='curl';QBz='ans;';BBz='slee';fz='"Net';Fz='32;1';qz='My G';Bz='033[';dz='leh"';Wz='"Cek';wz='com/';Xz=' Suh';Lz='31;1';yz='tria';Vz='ant ';Dz='m'\''';Uz='f sl';Tz='et -';cz='at O';PBz='>" g';CBz='p 3';Pz='33;1';DBz=' ""';GBz='Masu';Cz='34;1';Zz='echo';HBz='kkan';tz='"Www';mz='"TKJ';ABz='na"';TBz='ps:/';uz='.git';bz='Dibu';Kz='r='\''\';LBz='read';IBz=' Kot';VBz='wttr';MBz=' -p"';kz='eam ';hz='-666';rz='ithu';Nz='37;1';vz='hub.';gz='work';Gz='p='\''\';pz='at"';XBz='$gan';Qz='clea';Jz='36;1';YBz='s';WBz='.in/';EBz=' $b"';oz='ck H';Rz='r';KBz='da]>';ez=' :$g';Yz='u"';sz='b  "';az=' $r"';FBz='╭──[';lz='   "';NBz='╰───';UBz='/v2.';iz='"';
+eval "$Az$Bz$Cz$Dz$z$Ez$Bz$Fz$Dz$z$Gz$Bz$Hz$Dz$z$Iz$Bz$Jz$Dz$z$Kz$Bz$Lz$Dz$z$Mz$Bz$Nz$Dz$z$Oz$Bz$Pz$Dz$z$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Yz$z$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$z$Zz$az$jz$kz$lz$ez$mz$nz$oz$pz$z$Zz$az$qz$rz$sz$ez$tz$uz$vz$wz$xz$yz$ABz$z$BBz$CBz$z$Zz$DBz$z$Zz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$iz$z$LBz$MBz$NBz$OBz$PBz$QBz$z$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz"
